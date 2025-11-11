443c7015d40040122cb76ee69446c169d5d5c230
leechanhee <leechanhye3722@gmail.com>
2025-11-11 08:55:17 +0000

    update

--- active_fractal.py
+++ active_fractal.py
@@ -1,16 +1,18 @@
 import torch
 import torch.nn.functional as F
 
-def mix_fractal(x, fractal_batch, alpha: float = 0.2, active_lam: bool = False):
+def mix_fractal(x, fractal_batch, alpha: float = 0.2, active_lam: bool = False, retain_lam: bool = False):
     B, C, H, W = x.shape
     device, dtype = x.device, x.dtype
 
     overlay = fractal_batch
 
     if active_lam:
-        lam = torch.empty((B, 1), device=device, dtype=dtype).uniform_(0.1, 0.3)
-    else:
+        lam = torch.empty((B, 1), device=device, dtype=dtype).uniform_(0.15, 0.25)
+    elif not retain_lam:
         lam = torch.full((B, 1), alpha, device=device, dtype=dtype)
+    else:
+        lam = torch.full((B, 1), 1.0, device=device, dtype=dtype)
 
     mix_data = lam.view(B, 1, 1, 1) * overlay + (1 - lam.view(B, 1, 1, 1)) * x
     return mix_data, lam  # lam: (B, 1)
